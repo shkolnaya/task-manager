c072bbde9056b1b75e6963f06afb50427d1abd58
Angelina <lina.shkolnaya318@gmail.com>
2024-02-01 15:03:36 +0200

    implemented task component

--- src/app/user-account/tasks-page/task/task.component.ts
+++ src/app/user-account/tasks-page/task/task.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from './task.interface';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.scss']
 })
-export class TaskComponent {
+export class TaskComponent implements OnInit{
+
+  @Input()
+  task: Task;
+
+  ngOnInit(){}
 
 }
--- src/app/user-account/tasks-page/tasks-page.component.ts
+++ src/app/user-account/tasks-page/tasks-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from './task/task.interface';
 
 @Component({
   selector: 'app-tasks-page',
   templateUrl: './tasks-page.component.html',
   styleUrls: ['./tasks-page.component.scss']
 })
-export class TasksPageComponent {
+export class TasksPageComponent implements OnInit{
+
+  tasks: Task[] = [];
+
+  ngOnInit(): void {
+    this.tasks = [
+      {
+        name: 'My first task',
+        description: 'Create my firts task in this app',
+        category: 'General',
+      },
+      {
+        name: 'My second task',
+        description: 'Have my first task done',
+        category: 'General',
+      },
+      {
+        name: 'Clean the house',
+        category: 'Chores',
+      },
+    ]
+  }
 
 }
