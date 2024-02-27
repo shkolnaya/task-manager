05c7855bb73b103f87ff559022cca6abd009fc44
Angelina <lina.shkolnaya318@gmail.com>
2024-02-27 15:23:31 +0200

    divided tasks into two tables, added actions to selected tasks

--- src/app/user-account/projects-page/project-tasks/project-tasks.component.ts
+++ src/app/user-account/projects-page/project-tasks/project-tasks.component.ts
@@ -17,43 +17,92 @@ export class ProjectTasksComponent implements OnInit, AfterViewInit{
 
   constructor(private route: ActivatedRoute, private taskService: TaskService) { }
 
-
+  tasksToDo: Task[];
+  tasksDone: Task[];
   displayedColumns: string[] = ['select', 'name', 'description', 'date'];
-  dataSource: any;
+  toDoDataSource: any;
+  doneDataSource: any;
 
-  selection: SelectionModel<Task>;
+  toDoSelection: SelectionModel<Task>;
+  doneSelection: SelectionModel<Task>;
 
   ngOnInit() {
     this.route.params.subscribe(params=>{
       let id = params['id'];
       this.projectId = id;
-      this.tasks = this.taskService.getProjectTasks(id)
+      this.tasks = this.taskService.getProjectTasks(id);
     });
 
-    this.dataSource = new MatTableDataSource(this.tasks);
+    this.processData();
+
+    this.toDoDataSource = new MatTableDataSource(this.tasksToDo);
+    this.doneDataSource = new MatTableDataSource(this.tasksDone);
 
     const initialSelection = [] as Task[];
     const allowMultiSelect = true;
-    this.selection = new SelectionModel<Task>(allowMultiSelect, initialSelection);
+    this.toDoSelection = new SelectionModel<Task>(allowMultiSelect, initialSelection);
+    this.doneSelection = new SelectionModel<Task>(allowMultiSelect, initialSelection);
+  }
+
+  processData(){
+    this.getTasksToDo();
+    this.toDoDataSource = this.tasksToDo;
+    this.getTasksDone();
+    this.doneDataSource = this.tasksDone;
+  }
+
+  getTasksToDo(){
+    this.tasksToDo = this.tasks.filter(task =>{
+      return !task.isDone;
+      
+    })
+  }
+
+  getTasksDone(){
+    this.tasksDone = this.tasks.filter(task =>{
+      return task.isDone;
+    })
   }
 
   @ViewChild(MatSort) sort: MatSort;
 
   ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
+    this.toDoDataSource.sort = this.sort;
   }
 
     /** Whether the number of selected elements matches the total number of rows. */
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
+  isAllToDoSelected() {
+    const numSelected = this.toDoSelection.selected.length;
+    const numRows = this.toDoDataSource.data.length;
+    return numSelected == numRows;
+  }
+
+  isAllDoneSelected() {
+    const numSelected = this.doneSelection.selected.length;
+    const numRows = this.doneDataSource.data.length;
     return numSelected == numRows;
   }
 
   /** Selects all rows if they are not all selected; otherwise clear selection. */
   toggleAllRows() {
-    this.isAllSelected() ?
-        this.selection.clear() :
-        this.dataSource.data.forEach((row: Task) => this.selection.select(row));
+    this.isAllToDoSelected() ?
+        this.toDoSelection.clear() :
+        this.toDoDataSource.data.forEach((row: Task) => this.toDoSelection.select(row));
+  }
+
+  completeSelectedTasks(){
+    this.toDoSelection.selected.forEach(task  => {
+      task.isDone = true;
+    });
+    this.processData();
+    this.toDoSelection.clear();
+  }
+
+  undoneSelectedTasks(){
+    this.doneSelection.selected.forEach(task  => {
+      task.isDone = false;
+    });
+    this.processData();
+    this.doneSelection.clear();
   }
 }
